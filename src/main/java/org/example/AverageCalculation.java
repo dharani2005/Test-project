d1a9c4d3863416495663b3e390af8b03778c500b
dharani2005 <58557415+dharani2005@users.noreply.github.com>
2024-02-29 15:35:41 +0100

    executed the calculation part

---
+++ src/main/java/org/example/AverageCalculation.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class AverageCalculation {
+    public static void main(String[] args)
+    {
+    int result = (23+11+77)/3;
+    System.out.println("(23+11+77)/3 =" + result);
+}
+}
+
+
